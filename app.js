5da74d51296c3a5d2c915d5f7c7639d07e0a5af4
rakib-molla <rakibmail1997@gmail.com>
2024-09-10 23:32:44 +0600

    redirectiong request

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
 const http = require("http");
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-  const url = req.url;
+   const url = req.url;
+   const method = req.method;
+   console.log(url)
   if (url === "/") {
     res.setHeader("Content-Type", "text/html");
     res.write("<html>");
     res.write("<head><tile>Node js</tile></head>");
     res.write("<body> <form method='POST' action='/message'> <input type='text' name='name'/> <input type='submit' /></form> </body>");
     res.write("</html>");
+    console.log(res.statusCode)
     return res.end();
   }
-  
+
+  if(url === '/message' && method === 'POST'){
+   fs.writeFileSync('message.txt', 'dummy');
+   res.statusCode = 301;
+   res.setHeader('Location', '/');
+   return res.end();
+  }
+
   res.setHeader("Content-Type", "text/html");
     res.write("<html>");
     res.write("<head><tile>Node js</tile></head>");
