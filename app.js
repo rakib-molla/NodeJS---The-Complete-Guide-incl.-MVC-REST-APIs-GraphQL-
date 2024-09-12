dc1485e42e5a5051b29014c0313a5e11b9e563dc
rakib-molla <rakibmail1997@gmail.com>
2024-09-13 02:06:41 +0600

    static file send into cliend also read access to folder

--- app.js
+++ app.js
@@ -1,19 +1,27 @@
-const path = require('path');
 const express = require('express');
+const path = require('path');
+const rootDir = require('./utils/path');
+
 const bodyParser = require('body-parser');
 const app = express();
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended:false}))
+// send to static folder to front only this folder read access 
+app.use(express.static(path.join(rootDir, 'public')));
 
+
+// all routes 
 app.use('/admin',adminRoutes);
 app.use(shopRoutes);
 
+
 // send  404 not found page into frontend
 app.use((req, res, next)=>{
-  res.status(404).sendFile(path.join(__dirname, './views/404-not-found.html'));
+  res.status(404).sendFile(path.join(rootDir, 'views','404-not-found.html'));
 })
+
 
 app.listen(3000, ()=>{
   console.log('server is running on port: 3000')
