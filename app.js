281b50990e3c0efaffe55172f51c6b9aa6aac07f
rakib-molla <rakibmail1997@gmail.com>
2024-09-12 23:41:44 +0600

    middleware express js

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 const http = require("http");
-const routes = require('./routes');
 
-const server = http.createServer(routes);
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
 
-server.listen(3000, () => {
-  console.log("Server is running 3000");
-});
+app.use(bodyParser.urlencoded({extended:false}))
+
+
+app.use('/add-product', (req, res, next)=>{
+  res.send("<form action='/product' method='POST'><input type='text' name='title'/><button type='submit'>Add Product</button></form>")
+})
+
+app.post('/product', (req, res, next)=>{
+  console.log(req.body.title);
+  res.redirect('/');
+})
+app.use('/', (req, res, next)=>{
+  res.send("<h1>Hello From Express </h1>")
+})
+
+
+app.listen(3000, ()=>{
+  console.log('server is running on port: 3000')
+})
