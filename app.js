a720aacd66894ab0f17a11c4bc8f9089aba55b26
rakib-molla <rakibmail1997@gmail.com>
2024-09-13 00:11:13 +0600

    create separate routes and 404 not found page add using middleware and send response message

--- app.js
+++ app.js
@@ -3,22 +3,17 @@ const http = require("http");
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended:false}))
 
+app.use(adminRoutes)
+app.use(shopRoutes)
 
-app.use('/add-product', (req, res, next)=>{
-  res.send("<form action='/product' method='POST'><input type='text' name='title'/><button type='submit'>Add Product</button></form>")
+app.use((req, res, next)=>{
+  res.status(404).send("<h1 style='text-align:center'>Page Not Found</h1>")
 })
-
-app.post('/product', (req, res, next)=>{
-  console.log(req.body.title);
-  res.redirect('/');
-})
-app.use('/', (req, res, next)=>{
-  res.send("<h1>Hello From Express </h1>")
-})
-
 
 app.listen(3000, ()=>{
   console.log('server is running on port: 3000')
