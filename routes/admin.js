98be24640c17cabbd5c5c10bc7fdf7edadd0e237
rakib-molla <rakibmail1997@gmail.com>
2024-09-13 12:22:02 +0600

    output view dynamic content using pug html template engine

--- routes/admin.js
+++ routes/admin.js
@@ -11,7 +11,7 @@ router.get('/add-product', (req, res, next)=>{
  
 router.post('/add-product', (req, res, next)=>{
    product.push({title: req.body.title});
-   res.redirect('/');
+   res.redirect('/',);
  })
 
 module.exports.routes = router;
--- routes/shop.js
+++ routes/shop.js
@@ -6,8 +6,8 @@ const adminData = require('./admin');
 
 
 router.get('/', (req, res, next)=>{
-   console.log(adminData.product)
-   res.render('shop');
+   const products = adminData?.product;
+   res.render('shop',{products: products, docTitle:'Shop'});
 })
 
 
