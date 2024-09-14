b7de0230c9de865c3aafc314f8895b01675bc8b4
rakib-molla <rakibmail1997@gmail.com>
2024-09-15 01:05:37 +0600

    ejs template engine setup

--- app.js
+++ app.js
@@ -16,8 +16,6 @@ app.use(express.static(path.join(rootDir, 'public')));
 app.set('view engine', 'ejs');
 app.set('views','views');
 
-
-
 // set globally  for pug default html template engine 
 // app.set('view engine', 'pug');
 // app.set('views','views');
@@ -26,13 +24,11 @@ app.set('views','views');
 app.use('/admin',adminData.routes);
 app.use(shopRoutes);
 
-
 // send  404 not found page into frontend
 app.use((req, res, next)=>{
   // res.status(404).sendFile(path.join(rootDir, 'views','404-not-found.html'));
-  res.status(404).render('404-not-found', {pageTitle: "404 Not Found"})
+  res.status(404).render('404', {pageTitle: "404 Not Found"})
 })
-
 
 app.listen(3000, ()=>{
   console.log('server is running on port: 3000')
