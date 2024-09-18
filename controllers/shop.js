ed7b0c93f879626ce49b8b74e00152ae125b972b
rakib-molla <rakibmail1997@gmail.com>
2024-09-18 23:58:13 +0600

    adding another item orders

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,6 +1,5 @@
 const Product = require("../models/product");
 
-
 exports.getProducts = (req, res, next) => {
    const products = Product.fetchAll((products)=>{
      res.render('shop/product-list', {
@@ -25,6 +24,13 @@ exports.getCart = (req, res, next)=>{
    res.render('shop/cart',{
       path:'/cart',
       pageTitle: 'Your Cart'
+   });
+};
+
+exports.getOrders = (req, res, next)=>{
+   res.render('shop/orders',{
+      path:'/orders',
+      pageTitle: 'Your Orders'
    });
 };
 
