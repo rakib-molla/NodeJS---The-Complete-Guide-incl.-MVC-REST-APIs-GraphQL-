296b6a04cab6f05885d95b526cf5b59ddef095dd
rakib-molla <rakibmail1997@gmail.com>
2024-09-27 16:31:53 +0600

    add on to many realtion

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
 const sequelize = require('./util/database');
-
+const Product = require('./models/product');
+const User = require('./models/user');
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -13,6 +14,7 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -22,8 +24,11 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
+Product.belongsTo(User, {constraints: true, onDelete: 'CASCADE'});
+User.hasMany(Product);
+
 sequelize
-  .sync()
+  .sync({force: true})
   .then(result => {
     // console.log(result);
     app.listen(3000);
