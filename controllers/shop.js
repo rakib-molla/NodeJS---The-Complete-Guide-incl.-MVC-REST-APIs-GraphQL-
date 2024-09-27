2131b446e3c9e2d43171a517ec74fc7030a2f91f
rakib-molla <rakibmail1997@gmail.com>
2024-09-27 18:25:55 +0600

    deletiong related product and delete cart item

--- controllers/shop.js
+++ controllers/shop.js
@@ -2,6 +2,7 @@ const Product = require("../models/product");
 const Cart = require("../models/cart");
 const { where } = require("sequelize");
 
+
 exports.getProducts = (req, res, next) => {
   Product.findAll()
     .then((products) => {
@@ -78,14 +79,13 @@ exports.postCart = (req, res, next) => {
       if (products.length > 0) {
         product = products[0];
       }
-      
+
       if (product) {
         const oldQuantity = product.cartItem.quantity;
         newQuantity = oldQuantity + 1;
         return product;
       }
-      return Product.findByPk(prodId)
-      .catch((err) => console.log(err));
+      return Product.findByPk(prodId).catch((err) => console.log(err));
     })
     .then((product) => {
       return fetchedCart.addProduct(product, {
@@ -100,10 +100,21 @@ exports.postCart = (req, res, next) => {
 
 exports.postCartDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.findById(prodId, (product) => {
-    Cart.deleteProduct(prodId, product.price);
-    res.redirect("/cart");
-  });
+  req.user
+    .getCart()
+    .then((cart) => {
+      return cart.getProducts({ where: { id: prodId } });
+    })
+    .then((products) => {
+      const product = products[0];
+      return product.cartItem.destroy();
+    })
+    .then((result) => {
+      res.redirect("/cart");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
 
 exports.getOrders = (req, res, next) => {
