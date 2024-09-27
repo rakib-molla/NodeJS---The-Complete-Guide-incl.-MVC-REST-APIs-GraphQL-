2a2ffea014c25a2f159639c5ec23b3705a09b2ad
rakib-molla <rakibmail1997@gmail.com>
2024-09-27 18:15:52 +0600

    add product into cat

--- app.js
+++ app.js
@@ -45,8 +45,8 @@ Cart.belongsToMany(Product, {through: CartItem});
 Product.belongsToMany(Cart, {through: CartItem});
 
 sequelize
-  .sync({force: true})
-  // .sync()
+  // .sync({force: true})
+  .sync()
   .then((result) => {
     return User.findByPk(1);
   })
@@ -60,6 +60,9 @@ sequelize
     return user;
   })
   .then((user) => {
+    return user.createCart();
+  })
+  .then(cart=>{
     app.listen(3000);
   })
   .catch((err) => {
