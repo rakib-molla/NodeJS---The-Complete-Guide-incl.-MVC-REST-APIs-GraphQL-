13c39b3f30bcc47db4a380958b3c94eeeb1f0c72
rakib-molla <rakibmail1997@gmail.com>
2024-10-03 01:04:12 +0600

    product update

--- util/database.js
+++ util/database.js
@@ -1,15 +1,29 @@
 const mongodb = require('mongodb');
-
 const MongoClient = mongodb.MongoClient;
 
-const mongoConnect = (callback) =>{
-  MongoClient.connect('mongodb+srv://complete-node:complete-node@cluster0.clwxeiy.mongodb.net/').then((client)=>{
-    callback(client);
-    console.log("database connected")
-  }).catch((err)=>{
-    console.log(err)
-  })
-}
+let _db;
 
+const mongoConnect = callback => {
+  MongoClient.connect(
+    'mongodb+srv://complete-node:complete-node@cluster0.clwxeiy.mongodb.net/complete-node?retryWrites=true&w=majority&appName=Cluster0'
+  )
+    .then(client => {
+      console.log('Connected!');
+      _db = client.db();
+      callback();
+    })
+    .catch(err => {
+      console.log(err);
+      throw err;
+    });
+};
 
-module.exports = mongoConnect;
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw 'No database found!';
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
