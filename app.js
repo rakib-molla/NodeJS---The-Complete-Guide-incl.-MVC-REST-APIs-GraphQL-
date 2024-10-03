55b561cc899d438e6e8f34dc75a78bf9f28acfc2
rakib-molla <rakibmail1997@gmail.com>
2024-10-04 01:31:13 +0600

    storing multiple product in the cart

--- app.js
+++ app.js
@@ -21,7 +21,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
   User.findById('66fedb850bb86289299abd52')
     .then(user => {
-      req.user = user;
+      req.user = new User(user.name, user.email, user.cart, user._id);
+
       next();
     })
     .catch(err => console.log(err));
