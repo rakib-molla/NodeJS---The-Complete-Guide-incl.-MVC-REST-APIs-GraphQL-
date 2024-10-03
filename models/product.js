253c7effd98c44480a7d4504a4185e3675df8bc8
rakib-molla <rakibmail1997@gmail.com>
2024-10-04 00:10:57 +0600

    storing the user in your database

--- models/product.js
+++ models/product.js
@@ -2,12 +2,13 @@ const mongodb = require('mongodb');
 const getDb = require('../util/database').getDb;
 
 class Product {
-  constructor(title, price, description, imageUrl, id) {
+  constructor(title, price, description, imageUrl, id, userId) {
     this.title = title;
     this.price = price;
     this.description = description;
     this.imageUrl = imageUrl;
-    this._id = id;
+    this._id = id ? new mongodb.ObjectId(id) : null ;
+    this.userId = userId;
   }
 
   save() {
@@ -59,6 +60,18 @@ class Product {
         console.log(err);
       });
   }
+
+  static deleteById(prodId){
+    const db = getDb();
+    return db.collection('products').deleteOne({ _id: new mongodb.ObjectId(prodId) })
+    .then((result)=>{
+      console.log('Deleted');
+    })
+    .catch((err)=>{
+      console.log(err)
+    })
+  }
+
 }
 
 module.exports = Product;
