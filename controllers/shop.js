b39ef7ea970b011025703df7bf8ed6a149317bbc
rakib-molla <rakibmail1997@gmail.com>
2024-10-04 11:55:36 +0600

    display the cart items

--- controllers/shop.js
+++ controllers/shop.js
@@ -53,17 +53,12 @@ exports.getIndex = (req, res, next) => {
 exports.getCart = (req, res, next) => {
   req.user
     .getCart()
-    .then(cart => {
-      return cart
-        .getProducts()
-        .then(products => {
-          res.render('shop/cart', {
-            path: '/cart',
-            pageTitle: 'Your Cart',
-            products: products
-          });
-        })
-        .catch(err => console.log(err));
+    .then(products => {
+      res.render('shop/cart', {
+        path: '/cart',
+        pageTitle: 'Your Cart',
+        products: products
+      });
     })
     .catch(err => console.log(err));
 };
@@ -75,7 +70,7 @@ exports.postCart = (req, res, next) => {
     return req.user.addToCart(product);
   }).then(result=>{
     console.log(result)
-    
+    res.redirect('/cart');
   }).catch(err =>{
     console.log(err)
   })
