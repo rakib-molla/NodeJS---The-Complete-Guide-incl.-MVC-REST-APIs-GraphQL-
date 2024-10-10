4cf3deffbadf5e824c6b9a78787c66aee44f3ca1
rakib-molla <rakibmail1997@gmail.com>
2024-10-11 01:44:48 +0600

    session and cookie

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,14 +1,29 @@
-exports.getLogin = (req, res, next)=>{
-   // const isLoggedIn = 
-   console.log(req.get('Cookie'))
-   res.render('auth/login', {
-      path: '/login',
-      pageTitle: 'Login',
-      isAuthenticated: req.isLoggedIn
-   });
+const User = require("../models/user");
+
+exports.getLogin = (req, res, next) => {
+  res.render("auth/login", {
+    path: "/login",
+    pageTitle: "Login",
+    isAuthenticated: false,
+  });
 };
 
-exports.postLogin = (req, res, next)=>{
-   res.setHeader('Set-Cookie', 'loggedIn=true');
-   res.redirect('/');
-}
+exports.postLogin = (req, res, next) => {
+  User.findById("67057b443ef99307046b708c")
+    .then((user) => {
+      req.session.isLoggedIn = true;
+      req.session.user = user;
+      req.session.save((err) => {
+        console.log(err);
+        res.redirect("/");
+      });
+    })
+    .catch((err) => console.log(err));
+};
+
+exports.postLogout = (req, res, next) => {
+  req.session.destroy((err) => {
+    console.log(err);
+    res.redirect("/");
+  });
+};
